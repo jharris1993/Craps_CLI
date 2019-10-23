91e234cb0242dd0d46aecc506acb4e503854dfe6
jharris1993 <jimrh@charter.net>
2019-10-23 14:03:56 -0400

    update test_dice and .gitignore (why this is being tracked, I don't know)

--- test_dice.py
+++ test_dice.py
@@ -2,18 +2,41 @@
 # tests function throw_dice
 #
 import time
+import sys
 from throw_dice import throw_dice
 from display_dice import display_dice
 from is_win import is_win
 
-playmode = 1
-point = 10
+playmode = 0
+point = 0
 mylist = [0, 0, 0]
 
 mylist = throw_dice(6)
-mylist = [5, 5, 10]
+point = mylist[2]
+# mylist = [5, 5, 10]
+if playmode == 0:
+    print("\nCome-out roll. . .")
+elif playmode == 1 or playmode  == 2:
+    print("\nRolling for your point. . . .")
+else:
+    print("playmode =", playmode, "Oh Snap!  That's an invalid playmode!")
+    sys.exit(1)
+
 display_dice(mylist)
 winflag = (is_win(playmode, mylist, point))
+if playmode == 0:
+    if winflag == -1:
+        print("Sorry, you rolled a", mylist[2], "so you loose!")
+    elif winflag == 0:
+        print("Your point is", mylist[2])
+    elif winflag == 1:
+        print("Woo Hoo! You rolled a", mylist[2], "so YOU WIN!!")
+    else:
+        print("winflag =", winflag, "Oh Snap!  That's an invalid winflag!")
+        sys.exit(1)
+else:
+    pass
+
 print("\nPoint to make is", point, "\nYou rolled a", mylist[2])
 if winflag == -1:
     if playmode == 2 and mylist[0] == mylist[1]:
