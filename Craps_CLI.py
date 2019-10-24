e40880857b8bfca9eda7e4375d89a956aaa39d5d
jharris1993 <jimrh@charter.net>
2019-10-24 01:00:59 -0400

    Complete Craps_CLI, update keep_Playing

--- Craps_CLI.py
+++ Craps_CLI.py
@@ -7,13 +7,13 @@ from throw_dice import throw_dice
 from display_dice import display_dice
 from is_win import is_win
 from display_results import display_results
+from keep_playing import keep_playing
 
 texas_flag = 0  #  Rules to use: 0 = Vegas, 1 = Texas
 playmode = 0  #  Start game on come-out roll
 point = 0
 mylist = [0, 0, 0]
 point_to_make = 10
-keep_playing = 1  #  Assume the player will want to keep playing (default)
 
 while keep_playing == 1:
     mylist = throw_dice(6)  #  returns list of 3 numbers: die1, die2, total
@@ -39,31 +39,24 @@ while keep_playing == 1:
 #
 #  Keep Playing?
 #
-    if playmode == 0:
-        if winflag == 1 or winflag == 2:
-            print("Wow!  Must be lucky dice - let's keep going!\n")
+    play_flag = keep_playing(winflag, playmode, texas_flag)  #  Returns list[go_flag, playmode]
+
+    if play_flag[0] == 1:  #  play_flag[0] = the go flag. play_flag[1] = desired playmode
+        playmode = play_flag[1]
+        if winflag > 0:  #  Player won at something
+            print("\nExcellent!  These must be lucky dice, so let's keep playing!\n")
             continue
-        if winflag != -1:
-            if texas_flag == 1:
-                playmode = 2  #  Continue using *Texas* rules
-                continue
-            else:
-                playmode = 1  #  Continue using *Vegas* rules
-                continue
-    else:
-        if winflag == 1 or winflag == 2:
-            print("Wow!  Must be lucky dice - let's keep going!\n")
+        else:  #  winflag must be 0 because < 0 triggers a different go-flag
+            print("\nAll Right!  Now let's roll to make your point - Good Luck!\n")
             continue
-        elif winflag == 0:
-            print("Don't quit yet - the dice are just getting warm!")
+    else:  #  Go flag must be 0 so we ask to continue or not
+        keep_going = input("Would you like to play again? [Y] n\n")
+        if keep_going == "Y" or keep_going == "y" or keep_going == "":
+            playmode = 0
             continue
         else:
-            keep_going = input("Do you want to play again? [Y] n")
-            if keep_going == "y" or keep_going == "Y" or keep_going == "":
-                continue
-            else:
-                print("That's all folks!  Come back and play again soon!")
-                break
+            print("Thanks for playing!  I hope you come back soon!")
+            break
 #
 #  end of program
 #
--- keep_playing.py
+++ keep_playing.py
@@ -32,19 +32,31 @@
 #  Parameters:
 #    1.  Winflag  (To determine if the game should automatically continue or not.)
 #    2.  Playmode (To allow keep_playing to restart the game if necessary.)
+#    3.  Texas flag (to determine what play_mode to switch to after come-out roll)
 #
 #  Returns:
 #    1.  Goflag:  0 = end, 1 = keep going
 #    2.  Playmode:  Returns the playmode the game should continue in.
 #                   N/A If goflag = 0
 #
-def keep_playing(win_flag, play_mode):  #  Both parameters are mandatory and have no defaults
+def keep_playing(win_flag, play_mode, texas_flag):  #  Both parameters are mandatory and have no defaults
     """This function looks at both win_flag and play_mode and determines if the player should\n
         asked if they want to continue or not. Assume winning player wants to continue.\n"""
 #
-    go = 1
-#    stop = 0
+    go = 1  #  Constants to make things easier to read
+    ask = 0
 
-    if play_mode == 0 and win_flag > 0:  #  Player won on the come-out roll
+    if win_flag > 0:  #  Player won at something - therefore restart game if necessary
+        return (go, play_mode)  #  Restart using the same play mode
+    elif win_flag < 0:  #  Player lost, ask to restart, reset playmode
+        play_mode = 0
+        return (ask, play_mode)
+    elif play_mode == 0:  #  win_flag *MUST* be 0. If play_mode = 0, finished come-out roll
+        play_mode = (texas_flag + 1)  #  so we change the play_mode in accordance with texas flag
+#                                        (texas_flag = playmode - 1)
         return (go, play_mode)
-
+    else:  #  Both win_flag = 0 *AND* play_mode != 0 - i.e. continue a game in progress
+        return (go, play_mode)
+#
+#  end of function
+#
