3214a60d12f37c064bea9e3a7aa612f59be05ffb
jharris1993 <jimrh@charter.net>
2019-10-23 22:42:07 -0400

    update merge_squash to force focus to Dev branch

--- merge_squash.sh
+++ merge_squash.sh
@@ -28,7 +28,7 @@ git merge CLI_Dev
 echo -e "\ngit push origin master"
 git push origin master
 
-# forces active repository back to CLI_Dev.
+# force checkouot of CLI_Dev to restore focus to Dev branch
 # Absent this, I end up in "master"
 # which causes no end of trouble the next time
 # I try a commit - it pushes to master and not dev!
