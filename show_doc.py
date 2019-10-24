b1a9827368949fb06ec2e42ca93af649563540e5
jharris1993 <jimrh@charter.net>
2019-10-24 17:23:43 -0400

    show_doc:  Expanded stub

--- show_doc.py
+++ show_doc.py
@@ -2,3 +2,13 @@
 #
 #  Show doc is a small routine that displays what craps is, and what the rules are
 #  for those who are not familiar with the game.
+#
+#  Dependencies:  None
+#
+#  Parameters:    None
+#
+#  Returns:       zero (0)
+#
+def show_doc():
+    """This function displays what craps is, and what the rules are
+    for those not familiar with the game\n"""
